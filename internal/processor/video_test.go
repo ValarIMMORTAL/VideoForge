4911d6fe30f9eb26f920a37998ae3d1dbe97e29b
ssaicyo <2690186424@qq.com>
2025-04-02 22:26:59 +0800

    update 配置文件

--- internal/processor/video_test.go
+++ internal/processor/video_test.go
@@ -1,26 +1,19 @@
 package processor
 
-import (
-	"context"
-	"fmt"
-	"testing"
-	"time"
-)
-
-func TestGenerate(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel() // 确保资源释放
-
-	var arg = VideoParams{}
-	taskid, err := GenerateVideo(ctx, arg)
-	if err != nil {
-		return
-	}
-	fmt.Println("qidong")
-
-	//模拟主协程永不退出
-	for {
-
-	}
-	fmt.Println(taskid)
-}
+//func TestGenerate(t *testing.T) {
+//	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+//	defer cancel() // 确保资源释放
+//
+//	var arg = VideoParams{}
+//	taskid, err := GenerateVideo(ctx, arg)
+//	if err != nil {
+//		return
+//	}
+//	fmt.Println("qidong")
+//
+//	//模拟主协程永不退出
+//	for {
+//
+//	}
+//	fmt.Println(taskid)
+//}
